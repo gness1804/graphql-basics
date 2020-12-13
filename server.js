cae50b6ab6c211aa69f2e956286c4a36d304b11c
gness1804 <graham.nessler@gmail.com>
2020-12-13 15:02:06 -0600

    hook up graphql

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
 });
+
+app.use('/graphql', graphqlHTTP({
+  graphiql: true,
+}));
 
 app.listen(3000, () => {
   console.log('serving on port 3000.');
