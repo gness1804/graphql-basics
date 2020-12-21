5e337c742bfe3f36b46a1c9b2dc460344c13146f
Graham Nessler <grahamnessler@insiderx.com>
2020-12-21 15:16:23 -0600

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  plugins: [],
+  extends: [
+    'eslint:recommended',
+  ],
+  rules: {
+    'no-console': 'warn',
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+}
